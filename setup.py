97b7821b6dc3c4fee4da17a86e5323136ebfaffb
Zach Lü <zachlu@ZachdeMacBook-Air.local>
2024-11-17 20:32:44 +0800

    指定了redirect_uri

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ client_secret = os.environ["CLIENT_SECRET"]
 username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
 
-uri = ""
+uri = "localhost:8000/oauth/authorize"
 
 auth_client = OAuth2(
     client_id=client_id,
